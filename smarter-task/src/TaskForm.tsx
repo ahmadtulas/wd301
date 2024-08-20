ae6e7548f887083f963ddf7a2e61d9149dc9e406
Ahmad Jamal <ahmadjamalcs@gmail.com>
2024-08-20 16:16:57 +0530

    controlled component-Creating event handlers and form submission

--- smarter-task/src/TaskForm.tsx
+++ smarter-task/src/TaskForm.tsx
@@ -2,27 +2,38 @@ import React from "react";
 
 interface TaskFormProps {}
 
-interface TaskFormState {}
+interface TaskFormState {
+    title:string;
+}
 
 class TaskForm extends React.Component<TaskFormProps,TaskFormState> {
     constructor(props: TaskFormProps){
         super(props);
+        this.state ={
+            title:""
+        }
     }
 render()
 {
     return (
         <form onSubmit={this.addTask}>
-           <input type="text" className="border border-gray-300" ref={this.inputRef} />
+           <input type="text" className="border border-gray-300" value={this.state.title} onChange={this.titleChanged}/>
            <button type="submit" className="border border-balck-600">Add item</button>
         </form>
     )
 }
+titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) =>{
+    console.log(`er${event.target.value}`);
+    this.setState({title:event.target.value});
+}
+
 addTask: React.FormEventHandler<HTMLFormElement> = (event) =>{
     event.preventDefault();
     console.log("Submitted the Form");
-    console.log(`${this.inputRef.current?.value}`);
+    console.log(`${this.state.title}`);
+   
 }
-inputRef = React.createRef<HTMLInputElement>();
+
 
 }
 
