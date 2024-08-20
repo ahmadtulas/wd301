15d36f471a930aa049879869ceba0caa48c23ab5
Ahmad Jamal <ahmadjamalcs@gmail.com>
2024-08-20 16:06:47 +0530

    Creating event handlers and form submission-by using uncontrolled components

--- smarter-task/src/TaskForm.tsx
+++ smarter-task/src/TaskForm.tsx
@@ -11,12 +11,19 @@ class TaskForm extends React.Component<TaskFormProps,TaskFormState> {
 render()
 {
     return (
-        <form>
-           <input type="text" className="border border-gray-300" />
-           <button type="submit">Add item</button>
+        <form onSubmit={this.addTask}>
+           <input type="text" className="border border-gray-300" ref={this.inputRef} />
+           <button type="submit" className="border border-balck-600">Add item</button>
         </form>
     )
 }
+addTask: React.FormEventHandler<HTMLFormElement> = (event) =>{
+    event.preventDefault();
+    console.log("Submitted the Form");
+    console.log(`${this.inputRef.current?.value}`);
+}
+inputRef = React.createRef<HTMLInputElement>();
+
 }
 
 export default TaskForm
