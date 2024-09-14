c2c8ede0c4c1cdd0eddb9b69214baef18286b29a
Ahmad Jamal <ahmadjamalcs@gmail.com>
2024-09-14 16:13:10 +0530

    Introducing React Router

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,8 +1,24 @@
 import TaskApp from './TaskApp';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TaskListPage from './pages/TaskListPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/tasks',
+    element: <TaskListPage />,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <TaskApp />
+      <RouterProvider router={router} />
+      {/* <TaskApp /> */}
     </div>
   );
 }
