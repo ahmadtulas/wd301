4ff63693967eb81023cdfdff24ba795f1bce8e6c
Ahmad Jamal <ahmadjamalcs@gmail.com>
2024-09-18 14:46:52 +0530

    Handling Request with Async Await

--- smarter-tasks/src/ReactPlayground.tsx
+++ smarter-tasks/src/ReactPlayground.tsx
@@ -9,13 +9,30 @@ interface Post {
 const ReactPlayground = () => {
   const [data, setData] = useState<Post[]>([]);
 
+  //   useEffect(() => {
+  //     fetch('https://jsonplaceholder.typicode.com/posts')
+  //       .then((response) => response.json())
+  //       .then((data) => setData(data))
+  //       .catch((error) => console.error(error));
+  //   }, []);
+  //   console.log(data);
+
+  // async Request
+
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error(error));
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/posts'
+        );
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
   }, []);
-  console.log(data);
 
   return (
     <div className="max-w-4xl mx-auto p-4">
