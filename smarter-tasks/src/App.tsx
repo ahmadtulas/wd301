3442f6af45beb4c57e4e1b1c85bf8235ee61d4dc
Ahmad Jamal <ahmadjamalcs@gmail.com>
2024-09-24 11:25:49 +0530

    Creating our first context

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { RouterProvider } from 'react-router-dom';
+
+import ThemeContext from './context/theme';
 
 import router from './routes';
 
 const App = () => {
+  const currentTheme = useContext(ThemeContext);
   return (
     <div className="App">
+      {currentTheme}
       <RouterProvider router={router} />
       {/* <TaskApp /> */}
     </div>
--- smarter-tasks/src/main.tsx
+++ smarter-tasks/src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+import ThemeContext from './context/theme';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ThemeContext.Provider value="light">
     <App />
-  </React.StrictMode>
+  </ThemeContext.Provider>
 );
