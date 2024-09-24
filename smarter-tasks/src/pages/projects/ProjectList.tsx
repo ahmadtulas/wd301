1a64812163667f44fc961cea7751f4b2f888e6f7
Ahmad Jamal <ahmadjamalcs@gmail.com>
2024-09-24 14:22:44 +0530

    Action types: Incorporation of useReducer()

--- smarter-tasks/src/pages/projects/ProjectList.tsx
+++ smarter-tasks/src/pages/projects/ProjectList.tsx
@@ -1,12 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { API_ENDPOINT } from '../../config/constants';
 interface Project {
   id: number;
   name: string;
 }
-const ProjectList: React.FC = () => {
-  const [projects, setProjects] = useState<Project[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+interface State {
+  projects: Project[];
+  isLoading: boolean;
+}
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'API_CALL_START':
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case 'API_CALL_END':
+      return {
+        ...state,
+        isLoading: false,
+        projects: action.payload,
+      };
+    case 'API_CALL_ERROR':
+      return {
+        ...state,
+        isLoading: true,
+      };
+    default:
+      return state;
+  }
+};
+
+const ProjectList = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    projects: [],
+    isLoading: false,
+  });
+  //   const [projects, setProjects] = useState<Project[]>([]);
+  //   const [isLoading, setIsLoading] = useState<boolean>(false);
   useEffect(() => {
     // Fetch the list of projects here
     fetchProjects();
@@ -15,7 +52,8 @@ const ProjectList: React.FC = () => {
     const token = localStorage.getItem('authToken') ?? '';
 
     try {
-      setIsLoading(true);
+      //   setIsLoading(true);
+      dispatch({ type: 'API_CALL_START' });
       const response = await fetch(`${API_ENDPOINT}/projects`, {
         method: 'GET',
         headers: {
@@ -24,24 +62,32 @@ const ProjectList: React.FC = () => {
         },
       });
       const data = await response.json();
-      setProjects(data);
-      setIsLoading(false);
+      dispatch({ type: 'API_CALL_END', payload: data });
+      //   setProjects(data);
+      //   setIsLoading(false);
     } catch (error) {
       console.log('Error fetching projects:', error);
-      setIsLoading(false);
+      dispatch({ type: 'API_CALL_ERROR' });
+      //   setIsLoading(false);
     }
   };
   return (
     <div>
-      <h2>Project List</h2>
-      {isLoading ? (
-        <div>Loading...</div>
+      {state.isLoading ? (
+        <div>Loading...</div> // You can replace this with a progress bar component
       ) : (
-        <ul>
-          {projects.map((project) => (
-            <li key={project.id}>{project.name}</li>
+        <div className="grid gap-4 grid-cols-4 mt-5">
+          {state.projects.map((project) => (
+            <div
+              key={project.id}
+              className="block p-6 bg-green-50 border border-green-200 rounded-lg shadow hover:bg-green-100 dark:bg-green-800 dark:border-green-700 dark:hover:bg-green-700"
+            >
+              <h5 className="mb-2 text-xl font-medium tracking-tight text-green-900 dark:text-green-100">
+                {project.name}
+              </h5>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
