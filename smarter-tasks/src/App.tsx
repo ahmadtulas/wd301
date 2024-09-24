b9f9dbb96858110e79fb9a4b1c4fea291b02e1a5
Ahmad Jamal <ahmadjamalcs@gmail.com>
2024-09-24 12:35:26 +0530

    Introduction to useReducer-useState use case

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
-import ThemeContext from './context/theme';
+import { ThemeContext } from './context/theme';
 
 import router from './routes';
 
 const App = () => {
-  const currentTheme = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="App">
-      {currentTheme}
+    <div
+      className={`h-screen w-full mx-auto py-2 ${theme === 'dark' ? 'dark' : ''}`}
+    >
+      {theme}
       <RouterProvider router={router} />
       {/* <TaskApp /> */}
     </div>
--- smarter-tasks/src/main.tsx
+++ smarter-tasks/src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import ThemeContext from './context/theme';
+import { ThemeProvider } from './context/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeContext.Provider value="light">
+  <ThemeProvider>
     <App />
-  </ThemeContext.Provider>
+  </ThemeProvider>
 );
