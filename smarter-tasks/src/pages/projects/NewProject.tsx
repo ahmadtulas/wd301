769d145c4bf1db8031815d6fb8e03067eb12c571
Ahmad Jamal <ahmadjamalcs@gmail.com>
2024-09-25 14:01:03 +0530

    Intro to React hook form

--- smarter-tasks/src/pages/projects/NewProject.tsx
+++ smarter-tasks/src/pages/projects/NewProject.tsx
@@ -2,13 +2,21 @@ import { Dialog, Transition } from '@headlessui/react';
 
 import { Fragment, useState } from 'react';
 import { API_ENDPOINT } from '../../config/constants';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Inputs = {
+  name: string;
+};
 
 const NewProject = () => {
-  const [name, setName] = useState('');
   let [isOpen, setIsOpen] = useState(false);
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { name } = data;
     const token = localStorage.getItem('authToken') ?? '';
     try {
       const response = await fetch(`${API_ENDPOINT}/projects`, {
@@ -82,18 +90,17 @@ const NewProject = () => {
                     Create new project
                   </Dialog.Title>
                   <div className="mt-2">
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                       <input
                         type="text"
-                        required
                         placeholder="Enter project name..."
                         autoFocus
-                        name="name"
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        className="w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+                        {...register('name', { required: true })}
+                        className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+                          errors.name ? 'border-red-500' : ''
+                        }`}
                       />
+                      {errors.name && <span>This field is required</span>}
                       <button
                         type="submit"
                         className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
